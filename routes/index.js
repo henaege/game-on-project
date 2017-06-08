5f45381884173c495500be683c4f36fa41f31305
YINGRONG ZHAO <lionzhao0820@gmail.com>
2017-06-08 09:49:33 -0400

    user-page layout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var mysql = require('mysql');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { });
 });
-
+router.get('/user', function(req, res, next) {
+  res.render('user-page', { });
+});
 module.exports = router;
