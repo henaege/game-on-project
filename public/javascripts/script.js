b9e0eb5b6778c8fe353783501968ae97fc8a60db
YINGRONG ZHAO <lionzhao0820@gmail.com>
2017-06-12 08:13:14 -0400

    Save and edit buttons works

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -69,7 +69,7 @@ $(document).ready(function(){
     $(".edit-btn").click(()=>{
         var oldUsername = user
         $(".username-form-container").html("");
-        $(".username-form-container").html(`<form class='username-form' action='/changeUsername' method='post'><table class='table'><thead><tr><th class='text-center'> Your User Name </th><th class='text-center'> Action </th></tr></thead><tbody><td class="text-center"><input class="username-row" type="text" name="newUsername" value="${oldUsername}"></td><td class='text-center'><button class='btn btn-warning edit-btn'> Edit</button><button class='btn btn-primary type='submit'> Save</button></td></tbody></form>`);
+        $(".username-form-container").html(`<form class='username-form' action='/changeUsername' method='post'><table class='table modal-table table-hover'><thead><tr><th class='text-center'> Your User Name </th><th class='text-center'> Action </th></tr></thead><tbody><td class="text-center"><input class="username-row" type="text" name="newUsername" value="${oldUsername}"></td><td class='text-center'><button class='btn btn-warning edit-btn'> Edit</button><button class='btn btn-primary type='submit'> Save</button></td></tbody></form>`);
         $(".username-row").focus(function(){
             $(".username-row").attr("value", "");
         });
@@ -80,7 +80,7 @@ $(document).ready(function(){
     });
     $(".cancel-btn").click(()=>{
         $(".username-form-container").html("");
-        $(".username-form-container").html(`<form class='username-form' action='/changeUsername' method='post'><table class='table'><thead><tr><th class='text-center'> Your User Name </th><th class='text-center'> Action </th></tr></thead><tbody><td class="username-row text-center">${user}</td><td class='text-center'><button class='btn btn-warning edit-btn'> Edit</button><button class='btn btn-primary type='submit'> Save</button></td></tbody></form>`);
+        $(".username-form-container").html(`<form class='username-form' action='/changeUsername' method='post'><table class='table modal-table table-hover'><thead><tr><th class='text-center'> Your User Name </th><th class='text-center'> Action </th></tr></thead><tbody><tr class="text-center"></tr><td class="username-row text-center">${user}</td><td class='text-center'><button class='btn btn-warning edit-btn'> Edit</button><button class='btn btn-primary type='submit'> Save</button></td></tbody></form>`);
     });
 	
 Highcharts.chart('charts', {
